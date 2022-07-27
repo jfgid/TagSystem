a4699b91ca4e097cd2bcb754ae858c80ab644b5c
jfg <jeanf.giraud@gmail.com>
2022-07-27 22:25:26 +0200

    Put the definition of the range for computing the tag sequences in a separate header file.

---
+++ src/genTagSeqRange.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+#include <ranges>
+
+#include "genTag.hpp"
+#include "prodRules.hpp"
+
+template<ProdRuleFunc ProdRule, int DelNum, int MinLen>
+std::string genNextTag(const std::string& tag)
+{
+    if (tag.length() < MinLen) {
+        return "";
+    }
+    std::string nextTag = tag;
+    genTag<ProdRule>(DelNum, nextTag);
+    return nextTag;
+}
+
+template<size_t N>
+struct StringLiteral {
+    constexpr StringLiteral(const char(&str)[N]) {
+        std::copy_n(str, N, value);
+    }
+
+    char value[N];
+};
+
+template<ProdRuleFunc ProdRule, int DelNum, int MinLen, StringLiteral IniTag>
+struct TagSeqIter {
+    using difference_type = std::ptrdiff_t;
+
+    const std::string& operator*() const { return m_curTag; }
+
+    // Define prefix increment operator.
+    TagSeqIter& operator++() {
+        m_curTag = genNextTag<ProdRule, DelNum, MinLen>(m_curTag);
+        return *this;
+    }
+
+    // Define postfix increment operator.
+    TagSeqIter operator++(int) {
+        auto temp = *this;
+        ++*this;
+        return temp;
+    }
+
+    bool operator==(const TagSeqIter&) const = default;
+
+private:
+    std::string m_curTag = std::string(IniTag.value);
+};
+
+namespace views {
+    template<ProdRuleFunc ProdRule, int DelNum, int MinLen, StringLiteral IniTag>
+    auto tagSeq =
+        std::ranges::subrange<TagSeqIter<ProdRule, DelNum, MinLen, IniTag>, std::unreachable_sentinel_t>{};
+}
